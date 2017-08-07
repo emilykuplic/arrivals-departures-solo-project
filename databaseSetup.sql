2f3576011288e4db5a910f05c02f482a2aa9ab2c
Emily Kuplic <emilykuplic@gmail.com>
2017-08-07 16:48:13 -0500

    databaseSetup.sql updated database functioning correctly

--- databaseSetup.sql
+++ databaseSetup.sql
@@ -7,3 +7,27 @@ CREATE TABLE users (
     last_name character varying(35),
     role integer
 );
+
+CREATE TABLE events (
+    id serial PRIMARY KEY,
+    event_name character varying(50),
+    event_description character varying(500),
+    starting_date date,
+    ending_date date,
+);
+
+CREATE TABLE invitations (
+    id serial PRIMARY KEY,
+    event_id integer REFERENCES events,
+    user_id integer REFERENCES users,
+    arrival_date date,
+    departure_date date,
+    number_attending integer
+);
+
+Test Data
+INSERT INTO "events" (event_name, event_description, starting_date, ending_date) VALUES
+('Forth of July', 'Calling all Kuplics - it is firework time!', '06-29-2017', '07-09-2017');
+
+INSERT INTO "invitations" (event_id, user_id, arrival_date, departure_date, number_attending) VALUES
+(1, 1, '06-30-2017', '07-4-2017', 2);
